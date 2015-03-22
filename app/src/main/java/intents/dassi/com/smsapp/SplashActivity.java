fda312d81d5a666950fa6078f73a9a037fdddaa3
dassiorleando <dassi_orleando@yahoo.fr>
2015-03-22 11:26:08 +0100

    add of one SplashActivity, to show version and short description of lightSmsAndroid

---
+++ app/src/main/java/intents/dassi/com/smsapp/SplashActivity.java
@@ -0,0 +1,95 @@
+package intents.dassi.com.smsapp;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+
+public class SplashActivity extends ActionBarActivity {
+
+    private long ms=0;
+    private long splashTime=2000;
+    private boolean splashActive = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread splashThread = new Thread() {
+            @Override
+            public void run() {
+               try {
+                   while (splashActive && ms < splashTime) {
+                       ms = ms + 100;
+                       sleep(100);
+                   }
+
+               } catch(Exception e){ Log.d("trace trace trace trace", e.getMessage());}
+
+               finally {
+                   Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                   startActivity(intent);
+               }
+
+               }
+        };
+
+
+        splashThread.start();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_splash, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_splash, container, false);
+            return rootView;
+        }
+    }
+}
